82e4a47af91d21f639227b3d585d4108943112ca
Thomas Li Fredriksen <tfe@datarespons.no>
2017-04-20 14:34:45 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+
+"""
+
+__author__ = 'Thomas Li Fredriksen'
+__license__ = 'MIT'
+
+import setuptools
+from distutils.core import setup
+from codecs import open
+from os import path
+
+PWD = path.abspath(path.dirname(__file__))
+
+with open(path.join(PWD, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+data = dict(
+        name='PySwitch',
+        version='0.1.0',
+        url='https://github.com/thomafred/pywitch',
+        packages=['PySwitch'],
+        package_data={
+            'PySwitch' : [
+                'LICENSE',
+                'README.md'
+                ]
+            },
+        license='MIT',
+        description='Python switch implementation',
+        long_description=long_desc,
+        provides=['PySwitch'],
+        install_requires=required,
+        author='Thomas Li Fredriksen',
+        author_email='tom@lifredriksen.no',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Topic :: Software Development',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            ],
+        )
+
+if __name__ == '__main__':
+    setup(**data)
